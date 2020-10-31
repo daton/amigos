5eaa8e2eaa64211f6429bac86f85fb1d74659a39
juan_ <rapidclimate@gmail.com>
2020-10-30 19:49:06 -0600

    aa

--- src/main/java/org/localiza/amigos/amigos/AmigosApplication.java
+++ src/main/java/org/localiza/amigos/amigos/AmigosApplication.java
@@ -1,10 +1,14 @@
 package org.localiza.amigos.amigos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AmigosApplication {
+public class AmigosApplication implements CommandLineRunner {
+
+	@Autowired RepoHola repoHola;
 
 	public static void main(String[] args) {
 
@@ -12,4 +16,12 @@ public class AmigosApplication {
 
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//Esta es la famosa linea de comandos en spring
+		Hola h=new Hola();
+		h.setMensaje("Mi primer entidad a guardar en mongodb");
+		repoHola.save(h);
+
+	}
 }
