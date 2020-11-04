93c757239191c553746c2392e406322a81382e24
juan_ <rapidclimate@gmail.com>
2020-11-03 19:45:17 -0600

    aa

--- src/main/java/org/localiza/amigos/amigos/AmigosApplication.java
+++ src/main/java/org/localiza/amigos/amigos/AmigosApplication.java
@@ -8,20 +8,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AmigosApplication implements CommandLineRunner {
 
-	@Autowired RepoHola repoHola;
+@Autowired
+RepoHola repoHola;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(AmigosApplication.class, args);
+
 
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		//Esta es la famosa linea de comandos en spring
-		Hola h=new Hola();
-		h.setMensaje("Mi primer entidad a guardar en mongodb");
-		repoHola.save(h);
+	//	Hola h=new Hola();
+	//	h.setMensaje("Mi segundo mensajito de spring");
+		//De las opreaciones CRUD es es la "C" "create" o guardar o save
+		//repoHola.save(h);
+		//Ahora veremos desde aqui cómo leer o hacer una busqueda. (En BD SQL es SELECT)
+		  for(Hola h:repoHola.findAll()){
+			  System.out.println(h.getMensaje());
+		  }
 
 	}
 }
--- src/main/java/org/localiza/amigos/amigos/ControladorHola.java
+++ src/main/java/org/localiza/amigos/amigos/ControladorHola.java
@@ -2,20 +2,34 @@ package org.localiza.amigos.amigos;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
 public class ControladorHola {
+@Autowired RepoHola repoHola;
 
-    //Aqqui adentro van los metodos!! GET POST, ETC.
-    //Antes uno muuuy facil para sea su hola mundo de ARQITECTURA CON SERVICIOS REST
+//El metodo get se usa para obtener TODOS los registros guardados en la entidad en cuestion
     @GetMapping("/hola")
-    public String holaMundo(){
+    public List<Hola> holaMundo(){
 
-        return "MI PRIMER SERVICIO REST DE JAVA PARA EL MUNDO!!";
+        return repoHola.findAll();
     }
+ //El metodo get siguiente se ocupa para buscar un solo registro por su ID
+ @GetMapping("/hola/{id}")
+    public Hola buscarPorId (@PathVariable String id){
+        Hola h=new Hola();
+        if(repoHola.findById(id).isPresent()){
+            h=repoHola.findById(id).get();
+        }
+
+        return  h;
+ }
 
 }
