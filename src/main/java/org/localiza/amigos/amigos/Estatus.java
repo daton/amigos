976743835c1bfa5fe7446c2ee397e5701f1c3422
juan_ <rapidclimate@gmail.com>
2020-11-20 08:49:35 -0600

    ultima acttuaalizacion

--- src/main/java/org/localiza/amigos/amigos/Estatus.java
+++ src/main/java/org/localiza/amigos/amigos/Estatus.java
@@ -3,4 +3,20 @@ package org.localiza.amigos.amigos;
 public class Estatus {
     String mensaje;
     boolean success;
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
 }
